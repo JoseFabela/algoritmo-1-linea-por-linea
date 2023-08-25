4c27513eceb5e6826819e302df5866bb57b10b74
JoseFabela <jfabela522@gmail.com>
2023-08-25 00:46:40 -0600

    Agregar archivos de proyecto.

---
+++ algoritmo 1 linea por linea/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritmo_1_linea_por_linea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 12, 9, 20, 3, 15, 8 };
+
+            int maxNumber = FindMaxLinear(numbers);
+
+            Console.WriteLine($"The maximum number in the list is: {maxNumber}");
+            Console.ReadLine();
+
+        }
+        static int FindMaxLinear(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+                throw new InvalidOperationException("The list is empty.");
+
+            int max = numbers[0]; // Initialize max with the first element
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i]; // Update max if a larger number is found
+                }
+            }
+
+            return max;
+        }
+    }
+}
